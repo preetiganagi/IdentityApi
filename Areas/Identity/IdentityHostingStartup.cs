3f769a306c69105ae847cd421da841df69dd9387
Navya G <Navya G@LAPTOP-ETVIT14F>
2021-08-16 14:36:26 +0530

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IdentityApi.Areas.Identity.Data;
+using IdentityApi.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityApi.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityApi.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityApiContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityApiContextConnection")));
+
+                services.AddDefaultIdentity<IdentityApiUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityApiContext>();
+            });
+        }
+    }
+}
