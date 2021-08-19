e8ded383997897fa9457d73b7baba96e049805b9
Preeti Ganagi <pganagi@dealersocket.com>
2021-08-19 12:16:23 +0530

    added list users view

---
+++ Areas/Identity/Pages/Account/ListUsers.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityApi.Areas.Identity.Data;
+using IdentityApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApi.Areas.Identity.Pages.Account
+{
+    [Authorize]
+
+    public class ListUsersModel : PageModel
+    {
+
+        private readonly UserManager<IdentityApiUser> _userManager;
+
+        public ListUsersModel(UserManager<IdentityApiUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+        public IList<IdentityApiUser> User { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            User = await _userManager.Users.ToListAsync();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApi.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int IsActive { get; set; }
+        public string Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LirstName { get; set; }
+
+    }
+}
+
